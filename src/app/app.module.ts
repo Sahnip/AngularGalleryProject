9ecee3eecb6183bbcccff3e1cf34488963cc1308
Sahnip <pinhas.roumani@gmail.com>
2022-02-17 20:17:23 +0100

    Seconde commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,27 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HomeComponent } from './pages/home/home.component';
+import { HeaderComponent } from './features/header/header.component';
+import { FooterComponent } from './features/footer/footer.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    HeaderComponent,
+    FooterComponent
+  ],
+  imports: [
+    
+    AppRoutingModule,
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent],
+  exports: [HeaderComponent]
+})
+export class AppModule { }
